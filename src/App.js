b50d4435e9dce998ec59817242b688cee1cbf818
Anush Bhatia <an3208@srmist.edu.in>
2022-01-17 02:13:56 +0530

    minor bugs for deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import GlobalStyle from './globalStyles';
-import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <>
