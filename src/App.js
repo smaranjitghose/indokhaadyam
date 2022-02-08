7adc9ee718b618d8d99cd893cbf9672a493bbea8
mohak <vs6778502@gmail.com>
2022-02-08 12:44:17 +0530

    updated UI of footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import GlobalStyle from './globalStyles';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <>
