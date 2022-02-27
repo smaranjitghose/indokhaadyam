a80f7e2a52f0132e387e5a97962b78e940aa1441
Raja Majumdar <raja.majumdar.01@gmail.com>
2022-02-27 13:42:39 +0530

    feat: created the 404 Error Page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import ContactForm from "./components/Contact";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Team from "./pages/Team";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          {/* //* Home routes */}
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/contact" element={<ContactForm />} />
-            
-         {/* Features Page */}
-         {/* <Route exact path="/features" element={<Features/>} />  */}
-          <Route exact path="/team" element={<Team />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    {/* Page Not Found Error */}
+                    <Route exact path="*" element={<PageNotFound />} />
+
+                    {/* //* Home routes */}
+                    <Route exact path="/" element={<HomePage />} />
+                    <Route exact path="/contact" element={<ContactForm />} />
+
+                    {/* Features Page */}
+                    {/* <Route exact path="/features" element={<Features/>} />  */}
+                    <Route exact path="/team" element={<Team />} />
+                </Routes>
+            </Router>
+        </>
+    );
 };
 
 export default App;
-
