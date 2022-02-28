a2a3bbf2610a5766c21f3af66f4c63a24eaf3940
Raja Majumdar <raja.majumdar.01@gmail.com>
2022-02-28 16:29:38 +0530

    feat: added team page footer

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,6 +4,7 @@ import anush_bhatia from "../assets/anush_bhatia.png";
 import smaranjit_ghose from "../assets/smaranjit_ghose.jpg";
 import { Card } from "react-bootstrap";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Footer from "../components/Footer";
 
 function Team() {
   const maintainerSection = [
@@ -44,48 +45,51 @@ function Team() {
   }, []);
 
   return (
-    <div className="Team">
-      {/* <!-- Project Maintainer Sub Section --> */}
-      <p className="titleMain">Project Maintainers</p>
-      <div className="projectMaintainers">
-        {maintainerSection.map((item, index) => (
-          <Card key={index}>
-            <Card.Img variant="top" src={item.img} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <p className="maintainer_title">{item.title}</p>
-              <div className="logo_div">
-                <a href={item.githubLink} className="github_icon"><FaGithub/></a>
-                <a href={item.linkedinLink} className="linkedin_icon"><FaLinkedin/></a>
-              </div>
-            </Card.Body>
-          </Card>
-        ))}
+    <>
+      <div className="Team">
+        {/* <!-- Project Maintainer Sub Section --> */}
+        <p className="titleMain">Project Maintainers</p>
+        <div className="projectMaintainers">
+          {maintainerSection.map((item, index) => (
+            <Card key={index}>
+              <Card.Img variant="top" src={item.img} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <p className="maintainer_title">{item.title}</p>
+                <div className="logo_div">
+                  <a href={item.githubLink} className="github_icon"><FaGithub /></a>
+                  <a href={item.linkedinLink} className="linkedin_icon"><FaLinkedin /></a>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        {/* <!-- Contributors Sub Section --> */}
+        <p className="titleMain">Clan of Contributors</p>
+        <div className="team_container">
+          {contributors.map((c) => (
+            <figure
+              className="position_relative d_inline_block text_center grid_wd_100 figure"
+              key={c.login}
+            >
+              <img
+                src={c.avatar_url}
+                className="grid_wd_100 profile_img"
+                alt="Profile"
+              />
+              <figcaption className="position_absolute grid_wd_100 va_top">
+                <div className="fig_caption_text">
+                  <a href={c.html_url} target="_blank" rel="noreferrer">
+                    <FaGithub />
+                  </a>
+                </div>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
       </div>
-      {/* <!-- Contributors Sub Section --> */}
-      <p className="titleMain">Clan of Contributors</p>
-      <div className="team_container">
-        {contributors.map((c) => (
-          <figure
-            className="position_relative d_inline_block text_center grid_wd_100 figure"
-            key={c.login}
-          >
-            <img
-              src={c.avatar_url}
-              className="grid_wd_100 profile_img"
-              alt="Profile"
-            />
-            <figcaption className="position_absolute grid_wd_100 va_top">
-              <div className="fig_caption_text">
-                <a href={c.html_url} target="_blank" rel="noreferrer">
-                  <FaGithub />
-                </a>
-              </div>
-            </figcaption>
-          </figure>
-        ))}
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
