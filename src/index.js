d899b94d2e21528a74461d103ed2de5b33eaa16e
Shruti025 <shrutigoyal25042001@gmail.com>
2022-03-05 03:18:37 +0530

    Fastify and mongo updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-require('dotenv').config()
+const dotenv = require('dotenv').config()
 
-console.log(process.env.DB_URI);
+const fastify = require('fastify')({
+    logger: true
+})
+// database
+const mongoose = require('mongoose')
 
+// db connection
+mongoose.connect(process.env.DB_URI)
+.then(() => console.log('Mongo connection is working!'))
+.catch(err => console.log(err))
+
+fastify.get('/' , async(request, reply) => {
+    return {user: 'Hello User!'}
+})
+
+const start = async () => {
+    try{
+        await fastify.listen(3000)
+        fastify.log.info('Fastify works!')
+    }
+    catch (error){
+
+    }
+}
+
+start()
